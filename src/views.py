1a111cb82116d3b102670bbb8a4360bf7d74f63a
Aansh Kotian <76598799+AanshKot@users.noreply.github.com>
2023-02-19 15:05:39 -0500

    fixed about page, home.html

--- src/views.py
+++ src/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import Flask, render_template, url_for, redirect, request, session, jsonify, flash, Blueprint #importing the render template function
 
-from flask_login import login_required
+from flask_login import login_required,current_user
 
 from .fixer_call import fixer_call
 from .fixer_call import parse_data
@@ -12,7 +12,7 @@ views = Blueprint('views', __name__)
 
 @views.route("/")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", user = current_user)
 
 @views.route("/home",methods = ['POST','GET'])
 @login_required
@@ -28,6 +28,9 @@ def home():
 
         if(exchange_rate == "error"):
             flash("Please check inputted currency codes and try again", category="error")
+            
+            # session['user_in-currency'] = input_curr
+            # session['user-out_currency'] = output_curr
 
             
             return redirect(url_for("views.home")) 
