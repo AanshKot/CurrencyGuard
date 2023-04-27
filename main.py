8b34d352aaac1f11692f57c828a27565f4650547
Aansh Kotian <76598799+Aone52@users.noreply.github.com>
2023-04-27 11:32:40 -0400

    modified requirements.txt

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ app = create_app()
 #only when you run the main.py script directly, will __name =='main'
 if __name__ == '__main__':
     
-    app.run(debug = True)
+    app.run(host = "0.0.0.0",port = 5000,debug = True)
 
 
